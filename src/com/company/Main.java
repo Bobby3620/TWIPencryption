33cbfb0c3b52c9665d4bf8fa07fb81e66a9a4c01
rd568 <bobbyday3620@gmail.com>
2018-09-24 10:55:27 -0400

    ayyyyy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Global vars
+        Scanner kbInput = new Scanner(System.in);
+
+
+        //Where the user inputs a string
+        System.out.println("Type a string");
+        String kbWord = kbInput.nextLine();
+        int word = kbWord.length();
+
+
+
+
     }
 }
