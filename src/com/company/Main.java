109795298047532112885c84e813fdc7b003b679
Bobby3620 <41356385+Bobby3620@users.noreply.github.com>
2018-09-29 20:46:58 -0400

    aaaayyyyyyyyyyyyyy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,48 @@
 package com.company;
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
         // Global vars
         Scanner kbInput = new Scanner(System.in);
-
+        Random ranNum = new Random();
 
         //Where the user inputs a string
         System.out.println("Type a string");
         String kbWord = kbInput.nextLine();
-        int word = kbWord.length();
+        int wordLen = kbWord.length();
 
+        //Random number
+        int sp = (int)(wordLen * 0.10 + 1);
 
+        //To binary, octal, decimal, hex, ASCII
+        String a = Integer.toBinaryString(wordLen);//1
+        String b = Integer.toOctalString(wordLen);//2
+        int c = Integer.parseInt(a, 10);
+        String d = Integer.toHexString(wordLen);//4
+        char e = kbWord.charAt(4);
+        int ee = (int)e;//5
 
+        //Decides if it's binary, octal, decimal, hex, ASCII
+        for(int i = sp; i <= 5; i++){
+            if(i == 1){
+                System.out.print("The encryption of" + " " + kbWord + " is " + a);
+            }
+            else if(i == 2){
+                System.out.print(b);
+            }
+            else if(i == 3){
+                System.out.print(c);
+            }
+            else if(i == 4){
+                System.out.print(d);
+            }
+            else if(i == 5){
+                System.out.print(ee);
+            }
+        }
 
     }
 }
